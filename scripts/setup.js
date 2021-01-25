578d6c47a934e12390679f8043e9b787b9407364
Jean-Felix <jeanfelix.majurel@gmail.com>
2021-01-25 09:51:09 +0100

    disabled resizing feature

--- scripts/setup.js
+++ scripts/setup.js
@@ -5,8 +5,8 @@ document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 
 // We listen to the resize event
-window.addEventListener('resize', () => {
+/*window.addEventListener('resize', () => {
     // We execute the same script as before
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+});*/
