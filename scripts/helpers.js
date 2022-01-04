5175ea6a0b27e146d13ab4ecda47865fc67054cd
Jean-Felix <jeanfelix.majurel@gmail.com>
2022-01-04 22:56:31 +0100

    commited code from hostgator

--- scripts/helpers.js
+++
@@ -1,6 +0,0 @@
-function ajaxCall(url, method, contentType, body) {
-    var xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.setRequestHeader('Content-Type', contentType);
-    xhr.send(body);
-}
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,4 @@
 var NB_OF_PAGE = 5; 
-var EMAIL_SERVICE_URL = "https://localhost:8080/email";
 
 var nextArrow = document.getElementById("next-arrow");
 
@@ -7,16 +6,3 @@ nextArrow.addEventListener('click', () => window.scrollTo({
     top: document.body.scrollHeight/NB_OF_PAGE,
     behavior: 'smooth',
 }));
-
-var contactFrom = document.getElementById("contact-form");
-
-contactFrom.addEventListener("submit", function(evt) {
-    console.log("submit event received");
-    evt.preventDefault();
-
-    var email = document.getElementById("email");
-    var message = document.getElementById("message");
-
-    var body = encodeURI('email=' + email.value + '&message=' + message.value );
-    ajaxCall(EMAIL_SERVICE_URL, "POST", "application/x-www-form-urlencoded", body);
-});
